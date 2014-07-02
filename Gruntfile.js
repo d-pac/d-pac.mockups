98e73d515927b42900b902a69f8b0ed9013824db
Camille Reynders <camille.reynders@gmail.com>
2014-07-02 14:34:33 +0200

    Add release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,15 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
+var semver = require('semver');
+
 module.exports = function( grunt ){
 
     // Load grunt tasks automatically
-    require( 'jit-grunt' )( grunt );
+    require( 'jit-grunt' )( grunt, {
+        "bump-only"   : "grunt-bump",
+        "bump-commit" : "grunt-bump"
+    } );
 
     // Time how long tasks take. Can help when optimizing build times
     require( 'time-grunt' )( grunt );
@@ -36,6 +41,20 @@ module.exports = function( grunt ){
         ] );
     } );
 
-    grunt.registerTask('publish', ['gh-pages']);
+    grunt.registerTask( 'publish', ['gh-pages'] );
+
+    grunt.registerTask( 'release', function( versionOrType ){
+        var bumpTask = 'bump-only';
+        if( !versionOrType ){
+            bumpTask += ':patch';
+        }else if( semver.clean( versionOrType ) ){
+            grunt.option( 'setversion', versionOrType );
+        }else{
+            bumpTask += ':' + versionOrType;
+        }
+        grunt.task.run( [
+            bumpTask, 'version', 'build', 'bump-commit'
+        ] );
+    } );
 
 };
